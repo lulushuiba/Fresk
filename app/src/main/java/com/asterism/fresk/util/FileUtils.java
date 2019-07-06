5cce7dce44029949a0533f15fafc80e663d27053
ash <Glaxyinfinite@outlook.com>
2019-07-06 17:23:14 +0800

    添加util工具包和其类
    添加文件工具类
    添加日期工具类

--- app/src/main/java/com/asterism/fresk/util/FileUtils.java
+++ app/src/main/java/com/asterism/fresk/util/FileUtils.java
@@ -1,6 +1,20 @@
 package com.asterism.fresk.util;
 
+/**
+ * 文件工具类
+ *
+ * @author Ashinch
+ * @email Glaxyinfinite@outlook.com
+ * @date on 2019-07-06 17:19
+ */
 public class FileUtils {
+    /**
+     * 获取文件格式后缀名
+     *
+     * @param fileName 文件名
+     *
+     * @return 返回文件后缀名字符串（不包括点）
+     */
     public static String getSuffixNameByFileName(String fileName) {
         //去掉首尾的空格
         fileName = fileName.trim();
@@ -11,6 +25,13 @@ public class FileUtils {
         return "";
     }
 
+    /**
+     * 获取文件名称
+     *
+     * @param path 文件路径
+     *
+     * @return 返回文件名称字符串（不包括后缀名）
+     */
     public static String getFileNameByPath(String path) {
         //去掉首尾的空格
         path = path.trim();
